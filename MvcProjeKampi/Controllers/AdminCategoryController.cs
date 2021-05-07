100429861bbd118d6a5e26924afcbbc633850540
lenovo <iincefurkann@gmail.com>
2021-05-07 16:21:47 +0300

    V3

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        
+        public ActionResult Index()
+        {
+            var categoryvalues = categoryManager.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                categoryManager.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            
+            
+            return View();
+        }
+
+
+    }
+}
